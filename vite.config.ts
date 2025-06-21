cb177dc20ef160907a8963bbe890c8cd3b558501
Đorđe Jovanović <origami.sage@gmail.com>
2025-06-21 22:22:34 +0200

    feat: initialize new Tanstack Router(Vite) project

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import viteReact from '@vitejs/plugin-react'
+
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
