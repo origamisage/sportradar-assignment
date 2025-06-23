657eec91a9ab0f198eae3c258847b7feff7c9907
Đorđe Jovanović <origami.sage@gmail.com>
2025-06-23 18:13:04 +0200

    refactor: add custom chip component - Used Checkbox.Card as a base for implementing it - Currently supports `sm` and `md` size as well as `filled` variant

---
+++ src/components/CustomChip/CustomChip.tsx
@@ -0,0 +1,30 @@
+import { Checkbox } from '@mantine/core'
+import classes from './CustomChip.module.css'
+import type { CheckboxCardProps } from '@mantine/core'
+
+interface CustomChipProps extends CheckboxCardProps {
+  children: React.ReactNode
+  leftSide?: React.ReactNode
+  size?: 'sm' | 'md'
+  variant?: 'filled'
+}
+function CustomChip({ leftSide, size, children, ...rest }: CustomChipProps) {
+  return (
+    <Checkbox.Card
+      mod={{
+        size,
+      }}
+      classNames={{
+        card: classes.card,
+      }}
+      {...rest}
+    >
+      {leftSide}
+      {children}
+    </Checkbox.Card>
+  )
+}
+
+CustomChip.Group = Checkbox.Group
+
+export { CustomChip }
